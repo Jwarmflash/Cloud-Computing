aff9c63f7935bb82913847d597e24eb9a2fc2a26
Jwarmflash <157661346+Jwarmflash@users.noreply.github.com>
2024-06-28 17:40:22 -0700

    Add files via upload

---
+++ dataframe.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 16:31:38 2024
+
+@author: Jake Warmflash
+"""
+
+import sys
+import pandas as pd
+
+from pyspark.sql import SparkSession
+
+# Initialize SparkSession
+spark = SparkSession.builder.appName("Dataframe_PySpark").getOrCreate()
+
+# Create a simple local data.frame
+localDF = [("John", 19), ("Smith", 23), ("Sarah", 18)]
+
+# Convert local data frame to a SparkDataFrame
+df = spark.createDataFrame(localDF, ["name", "age"])
+
+# Print its schema
+df.printSchema()
+
+# Create a DataFrame from a JSON file
+path = "examples/src/main/resources/people.json".format(spark.sparkContext.getConf().get("spark.home"))
+peopleDF = spark.read.json(path)
+peopleDF.printSchema()
+
+# Register this DataFrame as a table.
+peopleDF.createOrReplaceTempView("people")
+
+# SQL statements can be run by using the sql methods
+teenagers = spark.sql("SELECT name FROM people WHERE age >= 13 AND age <= 19")
+
+
+# Call collect to get a local data frame
+teenagersLocalDF = teenagers.collect()
+
+# Print the teenagers in our dataset
+dfpd = pd.DataFrame(teenagersLocalDF)
+print(dfpd)
+
+# Stop the SparkSession now
+spark.stop()
